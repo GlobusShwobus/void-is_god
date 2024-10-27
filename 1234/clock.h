3591c88f023583d86bfc4518005b51fca7c5024b
lolw <ADMIN@DESKTOP-CCL0V71>
2024-10-27 14:36:32 +0200

    updated the timer and fixed the core logic,

--- 1234/clock.h
+++ 1234/clock.h
@@ -3,18 +3,24 @@
 #include <chrono>
 
 class Timer {
-	std::chrono::steady_clock::time_point last;
+	std::chrono::steady_clock::time_point time;
 
 public:
-
 	Timer() {
-		last = std::chrono::steady_clock::now();
+		time = std::chrono::steady_clock::now();
 	}
+	float diffSinceLast() {
+		const auto old = time;
+		time = std::chrono::steady_clock::now();
 
-	float mark() {
-		const auto old = last;
-		last = std::chrono::steady_clock::now();
-		const std::chrono::duration<float> difference = last - old;
-		return difference.count();
+		const std::chrono::duration<float> diff = time - old;
+
+		return diff.count();
+	}
+	float getTime() {
+		return std::chrono::duration<float>(std::chrono::steady_clock::now() - time).count();
+	}
+	void reset() {
+		time = std::chrono::steady_clock::now();
 	}
 };
