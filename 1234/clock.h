ae65bc20a81e8f66ea4da1db8b395b752e09551b
lolw <ADMIN@DESKTOP-CCL0V71>
2024-10-28 11:33:05 +0200

    removed diff since last in timer obj, it messed up the clock restarting

--- 1234/clock.h
+++ 1234/clock.h
@@ -9,14 +9,6 @@ public:
 	Timer() {
 		time = std::chrono::steady_clock::now();
 	}
-	float diffSinceLast() {
-		const auto old = time;
-		time = std::chrono::steady_clock::now();
-
-		const std::chrono::duration<float> diff = time - old;
-
-		return diff.count();
-	}
 	float getTime() {
 		return std::chrono::duration<float>(std::chrono::steady_clock::now() - time).count();
 	}
--- 1234/dude.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "vec2.h"
-
-class dude {
-	vec2i pos;
-	vec2i dimensions;
-	vec2i vel;
-
-};
