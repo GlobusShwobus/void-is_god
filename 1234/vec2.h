f7bd22ba7d8f364dd8c0a5f564b4012305425c94
lolw <ADMIN@DESKTOP-CCL0V71>
2024-10-28 10:42:00 +0200

    added vector2float class

--- 1234/vec2.h
+++ 1234/vec2.h
@@ -58,3 +58,62 @@ public:
 		return (x * x) + (y + y);
 	}
 };
+
+
+
+class vec2f
+{
+	float x = 0;
+	float y = 0;
+public:
+	vec2f(float X, float Y) :x(X), y(Y) {}
+	vec2f() = default;
+
+	bool operator==(const vec2f& rhs) {
+		return (this->x == rhs.x) && (this->y == rhs.y);
+	}
+	bool operator!=(const vec2f& rhs) {
+		return !(*this == rhs);
+	}
+
+	vec2f operator+(const vec2f& rhs)const {
+		return vec2f(x + rhs.x, y + rhs.y);
+	}
+	vec2f& operator+=(const vec2f& rhs) {
+		return *this = *this + rhs;
+	}
+
+	vec2f operator-(const vec2f& rhs)const {
+		return vec2f(x - rhs.x, y - rhs.y);
+	}
+	vec2f& operator-=(const vec2f& rhs) {
+		return *this = *this - rhs;
+	}
+
+	vec2f operator*(float rhs)const {
+		return vec2f(x * rhs, y * rhs);
+	}
+	vec2f& operator*=(float rhs) {
+		return *this = *this * rhs;
+	}
+
+	vec2f operator/(float rhs)const {
+		return vec2f(x / rhs, y / rhs);
+	}
+	vec2f& operator/=(float rhs) {
+		return *this = *this / rhs;
+	}
+
+
+	void scale(float _x) {
+		x *= _x;
+		y *= _x;
+	}
+
+	float getLengthSqrt() {
+		return std::sqrt((x * x) + (y + y));
+	}
+	float getLengthSq() {
+		return (x * x) + (y + y);
+	}
+};
