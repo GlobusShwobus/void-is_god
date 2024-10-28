a84eeb3b4c68e552dc3094cd62b9322b328a4ece
lolw <ADMIN@DESKTOP-CCL0V71>
2024-10-28 11:56:00 +0200

    updoot

--- 1234/main.cpp
+++ 1234/main.cpp
@@ -1,7 +1,6 @@
 //1. Start and finish chilis beginner course
-//2. learn chilis redpill videos
-//3. Start and learn intermediate
-//4. learn directX
+//2. introduce a graphics class which uses windows API to open a basic as fuck window
+//3. intorduce keyboard
 
 int main() {
 
