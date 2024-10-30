bcbdeaa029143dcef1b27fe5fb0ef36d36acffab
lolw <ADMIN@DESKTOP-CCL0V71>
2024-10-30 12:34:39 +0200

    rectF version gorrilion.5

--- 1234/RectF.h
+++ 1234/RectF.h
@@ -12,12 +12,15 @@ public:
 	float x2 = 0;
 	float y2 = 0;
 
+	RectF() = default;
+
 	RectF(float X1, float Y1, float X2, float Y2) :x1(X1), y1(Y1), x2(X2), y2(Y2) {}
 
-	RectF(const vec2f& posStart, float width, float height) :x1(posStart.x), y1(posStart.y), x2(x1 + width), y2(y1 + height) {}
+	RectF(const Vec2& posStart, float width, float height) :x1(posStart.x), y1(posStart.y), x2(x1 + width), y2(y1 + height) {}
 
-	RectF(const vec2f& topleft, const vec2f& bottomright) :x1(topleft.x), y1(topleft.y), x2(bottomright.x), y2(bottomright.y) {}
+	RectF(const Vec2& topleft, const Vec2& bottomright) :x1(topleft.x), y1(topleft.y), x2(bottomright.x), y2(bottomright.y) {}
 
+	RectF(const Vec2& center, float radius) :x1(center.x - radius), y1(center.y - radius), x2(center.x + radius), y2(center.y + radius) {}
 
 	float width()const {
 		return x2 - x1;
@@ -26,12 +29,8 @@ public:
 		return y2 - y1;
 	}
 
-	bool contains(const RectF& other) {
+	bool contains(const RectF& other)const {
 		return x1 < other.x2 && x2 > other.x1 && y1 < other.y2 && y2 > other.y1;
 	}
 
-
-	static RectF boxFromCenter(const vec2f& pos, int radius) {
-		return RectF(pos.x - radius, pos.y - radius, pos.x + radius, pos.y + radius);
-	}
 };
