a6c70c1c34bad83c192f071dd302e590c0f28771
lolw <ADMIN@DESKTOP-CCL0V71>
2024-10-30 10:18:08 +0200

    final version of rectF, not i get it

--- 1234/RectF.h
+++ 1234/RectF.h
@@ -2,34 +2,36 @@
 
 #include "vec2.h"
 
+// ONE THING IS FUCKY, IF X2 or Y2 < X1 or Y1 IT WILL STILL DRAW CORRECTLY BUT COLLISION WILL BE BROKEN
 class RectF {
 public:
-	float left = 0;	   //is the smallest x-value within the rectangle.
-	float top = 0;	   //is the smallest y-value within the rectangle.
-	float right = 0;   //is the largest x-value within the rectangle.
-	float bottom = 0;  //is the largest y-value within the rectangle.
 
+	float x1 = 0;
+	float y1 = 0;
 
+	float x2 = 0;
+	float y2 = 0;
 
-	RectF(float LEFT, float TOP, float RIGHT, float BOTTOM) :left(LEFT), top(TOP), right(RIGHT), bottom(BOTTOM) {}
+	RectF(float X1, float Y1, float X2, float Y2) :x1(X1), y1(Y1), x2(X2), y2(Y2) {}
 
-	RectF(const vec2f& pos, float width, float height) :left(pos.x), top(pos.y), right(pos.x + width), bottom(pos.y + height) {}
+	RectF(const vec2f& posStart, float width, float height) :x1(posStart.x), y1(posStart.y), x2(x1 + width), y2(y1 + height) {}
 
-	RectF(const vec2f& pos, const vec2f& size) :left(pos.x), top(pos.y), right(pos.x + size.x), bottom(pos.y + size.y) {}
+	RectF(const vec2f& topleft, const vec2f& bottomright) :x1(topleft.x), y1(topleft.y), x2(bottomright.x), y2(bottomright.y) {}
 
 
 	float width()const {
-		return right - left;
+		return x2 - x1;
 	}
 	float height()const {
-		return bottom - top;
+		return y2 - y1;
+	}
+
+	bool contains(const RectF& other) {
+		return x1 < other.x2 && x2 > other.x1 && y1 < other.y2 && y2 > other.y1;
 	}
 
 
-	bool contains(const RectF& other) {
-		return !(other.right <= left ||
-			other.left >= right ||
-			other.bottom <= top ||
-			other.top >= bottom);
+	static RectF boxFromCenter(const vec2f& pos, int radius) {
+		return RectF(pos.x - radius, pos.y - radius, pos.x + radius, pos.y + radius);
 	}
 };
