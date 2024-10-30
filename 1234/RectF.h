3f2ccbac7a9c37ea099484c97d4ceec5332c1e0c
lolw <ADMIN@DESKTOP-CCL0V71>
2024-10-30 12:35:20 +0200

    rectF version gorollion.6

--- 1234/RectF.h
+++ 1234/RectF.h
@@ -16,11 +16,11 @@ public:
 
 	RectF(float X1, float Y1, float X2, float Y2) :x1(X1), y1(Y1), x2(X2), y2(Y2) {}
 
-	RectF(const Vec2& posStart, float width, float height) :x1(posStart.x), y1(posStart.y), x2(x1 + width), y2(y1 + height) {}
+	RectF(const vec2f& posStart, float width, float height) :x1(posStart.x), y1(posStart.y), x2(x1 + width), y2(y1 + height) {}
 
-	RectF(const Vec2& topleft, const Vec2& bottomright) :x1(topleft.x), y1(topleft.y), x2(bottomright.x), y2(bottomright.y) {}
+	RectF(const vec2f& topleft, const vec2f& bottomright) :x1(topleft.x), y1(topleft.y), x2(bottomright.x), y2(bottomright.y) {}
 
-	RectF(const Vec2& center, float radius) :x1(center.x - radius), y1(center.y - radius), x2(center.x + radius), y2(center.y + radius) {}
+	RectF(const vec2f& center, float radius) :x1(center.x - radius), y1(center.y - radius), x2(center.x + radius), y2(center.y + radius) {}
 
 	float width()const {
 		return x2 - x1;
